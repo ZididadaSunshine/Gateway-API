259ca2c34e459bdf80a55569d1dbd91f50808c35
Anders Jakobsen <andersjkbsn@gmail.com>
2018-11-05 15:00:06 +0100

    Made authorization controller ready for tests

--- app/main/controller/authorization_controller.py
+++ app/main/controller/authorization_controller.py
@@ -3,25 +3,25 @@ from flask_restplus import Resource
 
 from app.main.decorator.auth_decorator import auth_required
 from app.main.dto.authorization_dto import AuthorizationDTO
-from app.main.service.authorization_service import AuthorizationResponse, login, logout
+import app.main.service.authorization_service as service
 
 api = AuthorizationDTO.api
 
 
 @api.route('/login')
 class Login(Resource):
-    @api.response(AuthorizationResponse.Success, 'Account login successful.')
-    @api.response(AuthorizationResponse.InvalidCredentials, 'Invalid or non-existing account credentials entered.')
+    @api.response(service.AuthorizationResponse.Success, 'Account login successful.')
+    @api.response(service.AuthorizationResponse.InvalidCredentials, 'Invalid or non-existing account credentials entered.')
     @api.doc('Perform a login request for an account.')
     @api.expect(AuthorizationDTO.authorization, validate=True)
     def post(self):
-        return login(request.json)
+        return service.login(request.json)
 
 
 @api.route('/logout')
 class Logout(Resource):
-    @api.response(AuthorizationResponse.Success, 'Logout successful.')
+    @api.response(service.AuthorizationResponse.Success, 'Logout successful.')
     @api.doc('Logout from a session.', security='jwt')
     @auth_required(api)
     def get(self):
-        return logout()
+        return service.logout()
