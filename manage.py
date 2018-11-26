1ac31e14034bfdf9c3cef3cef734ab4ec833cdfd
Anders Jakobsen <andersjkbsn@gmail.com>
2018-11-26 12:07:19 +0100

    Use SQLite for production env

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from flask_script import Manager
 from app import blueprint
 from app.main import create_app, db
 
-app = create_app('dev')
+app = create_app('prod')
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.register_blueprint(blueprint)
 app.app_context().push()
