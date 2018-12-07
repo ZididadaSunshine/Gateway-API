791502bfec655bff2e43a9559186993f3e5bff98
Anders Jakobsen <andersjkbsn@gmail.com>
2018-12-07 11:15:39 +0100

    Require authentication in statistics controller

--- app/main/controller/statistics_controller.py
+++ app/main/controller/statistics_controller.py
@@ -1,7 +1,10 @@
 from flask import request
 from flask_restplus import Resource
 
+from app.main.controller.authorization_controller import auth_required, get_token
 from app.main.dto.statistics_dto import StatisticsDTO
+from app.main.service import brand_service, synonym_service
+from app.main.service.authorization_service import get_account_id
 from app.main.service.statistics_service import get_statistics
 
 api = StatisticsDTO.api
@@ -9,9 +12,29 @@ api = StatisticsDTO.api
 
 @api.route('')
 class StatisticsResource(Resource):
-    @api.doc('Retrieve statistics from a time range.')
+    @api.doc('Retrieve statistics from a time range.', security='jwt')
     @api.expect(StatisticsDTO.timerange, validate=True)
+    @api.response(404, 'Requested brand does not exist or does not belong to the user.')
+    @api.response(400, 'Requested brand does not have any synonyms.')
+    @auth_required(api)
     def post(self):
-        response = get_statistics(request.json)
+        brand = brand_service.get_brand_by_id(get_account_id(get_token()), request.json['brand_id'])
 
-        return response.json(), response.status_code
+        if not brand:
+            api.abort(404)
+
+        # Get synonyms from brand
+        synonyms = [synonym.synonym for synonym in synonym_service.get_brand_synonyms(brand.id)]
+        if not synonyms:
+            api.abort(400)
+
+        request_data = {
+            'from': request.json['from'],
+            'to': request.json['to'],
+            'granularity': request.json['granularity'],
+            'synonyms': synonyms
+        }
+
+        statistics_response = get_statistics(request_data)
+
+        return statistics_response.json(), statistics_response.status_code
--- app/main/dto/statistics_dto.py
+++ app/main/dto/statistics_dto.py
@@ -7,7 +7,6 @@ class StatisticsDTO:
     timerange = api.model('Time range', {
         'from': fields.DateTime(required=True, description='The date and time which the time range spans from.'),
         'to': fields.DateTime(required=True, description='The date and time which the time range spans to.'),
-        'synonyms': fields.List(required=True, cls_or_instance=fields.String,
-                                description='List of synonyms to make statistics for.'),
+        'brand_id': fields.Integer(required=True, description='Brand identifier to retrieve statistics for.'),
         'granularity': fields.String(required=True, description='The granularity of the returned statistics')
     })
